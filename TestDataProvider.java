3fd7e872b96bd89af54316681a84662789f125c5
vladis18 <vladisbabayan@gmail.com>
2021-12-12 00:52:12 -0500

    Another one 12:52

--- TestDataProvider.java
+++ TestDataProvider.java
@@ -14,7 +14,7 @@ public class TestDataProvider {
         //System.out.println(something);
         System.out.println(" var 1 " + var + " var 2 " + var2);
         System.out.println(" Hello we are doing Jenkins");
-        System.out.println(" Another commit4");
+        System.out.println(" Another commit5");
     }
 
     @DataProvider
