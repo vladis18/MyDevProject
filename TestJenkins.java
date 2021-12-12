bb693aa4eef361f7bcbcde7493d575165e39fe1e
vladis18 <vladisbabayan@gmail.com>
2021-12-12 15:41:16 -0500

    Commiting TestJenkins class

---
+++ TestJenkins.java
@@ -0,0 +1,13 @@
+package com.hrms.testcases;
+
+
+
+public class TestJenkins {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
