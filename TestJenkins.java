7bdcb62bb6786a26f9aa91cd7ea538c25d22569c
vladis18 <vladisbabayan@gmail.com>
2021-12-12 16:11:03 -0500

    Hello from 4:10

--- TestJenkins.java
+++ TestJenkins.java
@@ -5,7 +5,7 @@ package com.hrms.testcases;
 public class TestJenkins {
     public static void main(String[] args) {
         for(int i=0;i<=5;i++){
-            System.out.println("hello00000 " + i);
+            System.out.println("hello00000 +++++" + i);
         }
 
     }
