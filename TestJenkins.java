d43237b518bb5c2a7f9847334ca5ec6d6daa820d
vladis18 <vladisbabayan@gmail.com>
2021-12-12 15:45:07 -0500

    Commit 3:45

--- TestJenkins.java
+++ TestJenkins.java
@@ -5,7 +5,7 @@ package com.hrms.testcases;
 public class TestJenkins {
     public static void main(String[] args) {
         for(int i=0;i<=5;i++){
-            System.out.println(i);
+            System.out.println("hello " + i);
         }
 
     }
