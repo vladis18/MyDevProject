1e78b9598c6b9827ed6828b398c4c61304a19dcf
vladis18 <vladisbabayan@gmail.com>
2021-12-12 16:35:20 -0500

    Hello

--- TestJenkins.java
+++ TestJenkins.java
@@ -5,7 +5,7 @@ package com.hrms.testcases;
 public class TestJenkins {
     public static void main(String[] args) {
         for(int i=0;i<=5;i++){
-            System.out.println("hello00000 +8888" + i);
+            System.out.println("I love you"  + i);
         }
 
     }
